d089c4e5cb58495b0b57a2e567c3f8d92025c930
sijanghm <sijan.ghimire29@gmail.com>
2021-12-06 17:05:09 +0545

    chqanges

--- src/main/java/com/sijan/controller/StudentController.java
+++ src/main/java/com/sijan/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.sijan.controller;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import com.sijan.model.Student;
 import com.sijan.service.StudentService;
